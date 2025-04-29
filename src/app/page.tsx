aae5a42560806a7036f88a0c6155fceaddecb551
Diego Santos <diegosantos.engtads@gmail.com>
2025-04-28 21:15:43 -0300

    feat: initial config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { PostList } from "@/components/PostList";
+import { SpinLoader } from "@/components/SpinLoader";
+import { Suspense } from "react";
+
 export default function HomePage() {
-    return <h1>Retorno da HomePage</h1>;
+  return (
+    <>
+      <header>
+        <h1>Aqui é a Header</h1>
+      </header>
+      <Suspense fallback={<SpinLoader />}>
+        <PostList />
+      </Suspense>
+      <footer>
+        <h3>Aqui é a Footer</h3>
+      </footer>
+    </>
+  );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export default function Header() {
+  console.log("HEZADER");
+  return <h1>Aqui é {"Use Client"}</h1>;
+}
