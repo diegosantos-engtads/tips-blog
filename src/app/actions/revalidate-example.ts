99b81629228b2cc6d5926210884b27f1a8fbc73f
Diego Santos <diegosantos.engtads@gmail.com>
2025-05-27 18:44:49 -0300

    feat: config gitignore and created actions

---
+++ src/app/actions/revalidate-example.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function revalidateExampleAction() {
+  console.log("Estou em uma server action");
+}
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -57,5 +57,4 @@ export class JsonPostRepository implements PostRepository {
 
     return post;
   }
-
 }
